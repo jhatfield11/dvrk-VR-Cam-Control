78ffcd0e7d5cc4438f2385ebbbe2dd0a5f590992
James Hatfield <jhatfield3@sheffield.ac.uk>
2021-03-14 12:33:34 +0000

    Initial Commit

---
+++ ROS-dvrk-VR/scripts/Base_subscriber.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+    
+"""
+import rospy
+import dvrk 
+import tf_conversions
+import tf2_ros
+import geometry_msgs.msg
+
+from geometry_msgs.msg import Pose
+from moveit_msgs.msg import RobotTrajectory
+
+
+def callback(data, arm):
+    br = tf2_ros.TransformBroadcaster()
+    t = geometry_msgs.msg.TransformStamped()
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = "world"
+
+    if  (arm == "PSM1" )| (arm == "PSM2"):
+        t.child_frame_id = arm + "_psm_base_link"
+    else:
+        t.child_frame_id = "ecm_setup_base_link"
+
+    t.transform.translation = data.position
+    t.transform.rotation = data.orientation
+
+    br.sendTransform(t)
+
+if __name__ == '__main__':
+    rospy.init_node('Base_Subscriber', anonymous=True)
+    arm = rospy.get_param('~arm')
+    rospy.Subscriber("/unity/%s/base_frame_output/" % arm, Pose, callback, arm)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
---
+++ ROS-dvrk-VR/scripts/server_endpoint.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import rospy
+
+
+from ros_tcp_endpoint import TcpServer, RosPublisher, RosSubscriber, RosService
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import JointState
+
+
+def main():
+    ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
+    tcp_server = TcpServer(ros_node_name)
+    rospy.init_node(ros_node_name, anonymous=True)
+
+    # Start the Server Endpoint with a ROS communication objects dictionary for routing messages
+    tcp_server.start({
+        
+        #"/unity/ECM/base_frame/": RosPublisher('/dvrk/ECM/set_base_frame/', Pose, queue_size=10),
+        #"/unity/PSM1/base_frame/": RosPublisher('/dvrk/PSM1/set_base_frame/', Pose, queue_size=10),
+        #"/unity/PSM2/base_frame/": RosPublisher('/dvrk/PSM2/set_base_frame/', Pose, queue_size=10),
+        "/unity/ECM/base_frame/": RosPublisher('/unity/ECM/base_frame_output/', Pose, queue_size=10),
+        "/unity/PSM1/base_frame/": RosPublisher('/unity/PSM1/base_frame_output/', Pose, queue_size=10),
+        "/unity/PSM2/base_frame/": RosPublisher('/unity/PSM2/base_frame_output/', Pose, queue_size=10),
+        
+
+        '/unity/Left_Controller/pose/': RosPublisher('/dvrk/PSM1/set_position_goal_cartesian', Pose, queue_size=10),
+        '/unity/Right_Controller/pose/': RosPublisher('/dvrk/PSM2/set_position_goal_cartesian', Pose, queue_size=10),
+
+        '/dvrk/PSM1/joint_states/': RosSubscriber('/dvrk/PSM1/joint_states/', JointState, tcp_server),
+        '/dvrk/PSM2/joint_states/': RosSubscriber('/dvrk/PSM2/joint_states/', JointState, tcp_server),
+        '/dvrk/ECM/joint_states/': RosSubscriber('/dvrk/ECM/joint_states/', JointState, tcp_server),
+
+    })
+    
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
