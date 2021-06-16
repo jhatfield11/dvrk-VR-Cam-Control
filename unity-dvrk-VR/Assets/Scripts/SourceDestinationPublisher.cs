1643b65c4d10d335da4131d0bf028e978379cee5
jhatfield11 <47004262+jhatfield11@users.noreply.github.com>
2021-06-16 19:46:26 +0100

    Most Recent Changes/Final Commit

--- unity-dvrk-VR/Assets/Scripts/SourceDestinationPublisher.cs
+++ unity-dvrk-VR/Assets/Scripts/SourceDestinationPublisher.cs
@@ -1,35 +1,45 @@
 using RosMessageTypes.Geometry;
 using RosMessageTypes.Sensor;
 using RosMessageTypes.NiryoMoveit;
+using RosMessageTypes.ROS;
 using UnityEngine;
 using ROSGeometry;
 using Quaternion = UnityEngine.Quaternion;
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.IO;
+using System.Linq;
 
 public class SourceDestinationPublisher : MonoBehaviour
 {
     // ROS Connector
     private ROSConnection ros;
 
+    public string Path;
+    public string fileName;
+    public bool DisplayPointCloud;
+
+    public Color color;
+    public float particleSize = 5;
+
     // Variables required for ROS communication
-    public string[] joint_topics;
+    private string[] joint_topics;
     public string[] frame_topics;
     public string[] controller_topics;
 
     public GameObject[] Arms;
-    public GameObject[] Base_Frames = new GameObject[3];
+    private GameObject[] Base_Frames = new GameObject[3];
     public GameObject[] Controllers;
 
-    public List<string> joint_Names;
+    private List<string> joint_Names;
     private List<double> current_Angles;
     private List<double> current_Vel;
     private List<double> current_Effort;
 
-    public int i_ECM;
-    public int i_PSM2;
-    public int i_PSM1;
+    private int i_ECM;
+    private int i_PSM2;
+    private int i_PSM1;
 
     public GameObject _camera;
     
@@ -45,6 +55,7 @@ public class SourceDestinationPublisher : MonoBehaviour
         RosMessageTypes.Geometry.Pose[] position_message = new RosMessageTypes.Geometry.Pose[Controllers.Length];
     // Get ROS connection static instance
         ros = ROSConnection.instance;
+        parseXYZ(ros, Path + fileName);
         int i = 0;
         
         joint_topics = new string[Arms.Length];
@@ -144,5 +155,60 @@ public class SourceDestinationPublisher : MonoBehaviour
     Quaternion Relative(Quaternion a, Quaternion b)
     {
         return Quaternion.Inverse(a) * b;
+    }
+    
+    void parseXYZ(ROSConnection rs,string filePath)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+        double[] X = new double[lines.Length];
+        double[] Y = new double[lines.Length];
+        double[] Z = new double[lines.Length];
+        int i = 0;
+        foreach (string line in lines)
+        {
+            var words = line.Split(' ');
+            X[i] = (double.Parse(words[0]) / 1000);
+            Y[i] = (double.Parse(words[1]) / 1000);
+            Z[i] = (double.Parse(words[2]) / 1000);
+            i++;
+        }
+        RosMessageTypes.ROS.XYZcloud Cloud = new RosMessageTypes.ROS.XYZcloud();
+        Cloud.X = X;
+        Cloud.Y = Y;
+        Cloud.Z = Z;
+
+        if (DisplayPointCloud == true)
+        {
+            ApplyToParticleSystem(X, Y, Z);
+        }
+        else
+        {
+            var ps = GetComponent<ParticleSystem>();
+            ps.Stop();
+        }
+
+        rs.Send("/unity/XYZCloud", Cloud);
+    }
+    public void ApplyToParticleSystem(double[] x, double[] y, double[] z)
+    {
+        var ps = GetComponent<ParticleSystem>();
+        if (ps == null)
+        {
+            Debug.Log("Particle system not created");
+            return;
+        }
+
+
+        var particles = new ParticleSystem.Particle[x.Length];
+
+        for (int i = 0; i < particles.Length; ++i)
+        {
+            particles[i].position = new UnityEngine.Vector3((float)x[i], (float)y[i], (float)z[i]);
+            particles[i].startSize = particleSize;
+            particles[i].startColor = color;
+        }
+        ps.SetParticles(particles);
+        ps.Pause();
+        Debug.Log("Particle system created");
     }
 }
